1bfec27472cc1e579f6fa52f3fecedfef89f7eb3
Rimshadek1 <rmzrimshad1@gmail.com>
2023-07-08 15:59:43 +0530

    mangodb connected,react connected,node connected,all inter connected

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,42 @@
 const express = require("express");
 const app = express();
+var indexRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
 const cors = require('cors');
+var db = require('./config/connection');
+
+
+
+
+
+
 app.use(cors());
+app.use('/', indexRouter);
+app.use('/admin', adminRouter);
+
+
+
+
+db.connect((err) => {
+    if (err)
+        console.log('errrorrrr' + err);
+    else
+        console.log("Database connected");
+})
+
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+
 
 app.post("/post", (req, res) => {
     console.log("Connected to React");
